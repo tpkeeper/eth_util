043c01f6b650e300028536e84df0ffdd5a9a74b7
tpkeeper <tpkeeper.me@gmail.com>
2020-08-10 14:42:43 +0800

    add tg bot

---
+++ cmd/bot/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const (
+	addMonitorStep            = "addMonitor"
+	addContractAddressStep    = "addContractAddress"
+	addTokenAddressStep       = "addTokenAddress"
+	deleteMonitorStep         = "deleteMonitor"
+	deleteContractAddressStep = "deleteContractAddress"
+	deleteTokenAddressStep    = "deleteTokenAddress"
+	listMonitorStep           = "listMonitor"
+)
+
+var step []string
+var contractTarget = make(map[string][]string)
+var tempContractAddress string
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("1155247913:AAEDAErTT4YwixdFbu-ZSSdDF6przCjZh30")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	var mainMenu = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Add Monitor", addMonitorStep),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Delete Monitor", deleteMonitorStep),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("List Monitor", listMonitorStep),
+		),
+	)
+
+	var deleteMenu = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Delete by contract address", deleteContractAddressStep),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Delete by token address", deleteTokenAddressStep),
+		),
+	)
+
+	for update := range updates {
+
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			if update.Message.IsCommand() {
+				switch update.Message.Command() {
+				case "start":
+					msg.Text = "hello! I`m tpkeeper`s bot.\n/menu to show the main menu"
+				case "menu":
+					msg.Text = "main menu:"
+					msg.ReplyMarkup = mainMenu
+				default:
+					msg.Text = "sorry,this command not exist!"
+				}
+				bot.Send(msg)
+			} else {
+
+				if len(step) == 0 {
+					msg.Text = "not a command"
+					bot.Send(msg)
+					continue
+				}
+
+				switch step[len(step)-1] {
+				case addContractAddressStep:
+
+					tempContractAddress = update.Message.Text
+					msg.Text = "please enter token address"
+					_, err := bot.Send(msg)
+					if err == nil {
+						step = append(step, addTokenAddressStep)
+					}
+
+				case addTokenAddressStep:
+
+					step = append(step, addTokenAddressStep)
+					contractTarget[tempContractAddress] = append(contractTarget[tempContractAddress],
+						update.Message.Text)
+
+					msg.Text = fmt.Sprintf("add monitor ok!\ncontract address: %s \ntoken address: %s",
+						tempContractAddress, update.Message.Text)
+					bot.Send(msg)
+				case deleteMonitorStep:
+				case listMonitorStep:
+				default:
+					step = []string{}
+					msg.Text = "bad step, return main menu"
+					msg.ReplyMarkup = mainMenu
+					bot.Send(msg)
+				}
+
+			}
+		}
+
+		if update.CallbackQuery != nil {
+			bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data))
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
+
+			switch update.CallbackQuery.Data {
+			case addMonitorStep:
+				msg.Text = "please enter contract address"
+				_, err := bot.Send(msg)
+				if err == nil {
+					step = append(step, addContractAddressStep)
+				}
+			case deleteMonitorStep:
+				msg.Text = "please select one:"
+				msg.ReplyMarkup = deleteMenu
+				_, err := bot.Send(msg)
+				if err != nil {
+				}
+			case listMonitorStep:
+			default:
+				step = []string{}
+				msg.Text = "bad step, return main menu"
+				msg.ReplyMarkup = mainMenu
+				bot.Send(msg)
+			}
+
+		}
+	}
+
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/ethereum/go-ethereum v1.9.18
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/miguelmota/go-ethereum-hdwallet v0.0.0-20200123000308-a60dcd172b4c
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344
 )
